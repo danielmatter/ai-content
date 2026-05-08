359508824e874c6efa064ec290fdbaa9cb9be188
DanielMatter <matter96@gmail.com>
2026-05-08 09:59:12 +0200

    Disable Registration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
 
 import { db } from "@/lib/db/client";
+import { APIError, createAuthMiddleware } from "better-auth/api";
 
 const authBaseURL = process.env.BETTER_AUTH_URL ?? "http://localhost:3001";
 
@@ -22,4 +23,17 @@ export const auth = betterAuth({
     autoSignIn: true,
   },
   plugins: [nextCookies()],
+  hooks: {
+    before: createAuthMiddleware(async (ctx) => {
+      if (process.env.ALLOW_REGISTRATION == "true") {
+        return;
+      }
+
+      if (ctx.path === "/sign-up/email") {
+        throw new APIError("FORBIDDEN", {
+          message: "Registration is disabled",
+        });
+      }
+    }),
+  },
 });
