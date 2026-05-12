bc164025b1c0523e8a51cbf615dc44d3e2c0fe3a
DanielMatter <matter96@gmail.com>
2026-05-12 20:03:16 +0200

    Rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/dietz-en",
+        destination:
+          "https://dietz-verlag.de/isbn/9783801231118/The-Open-Future-and-Its-Enemies-How-we-can-Protect-Free-Society-from-AI-Dictatorship-Matthias-Pfeffer-Juergen-Pfeffer-Paul-Nemitz",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
